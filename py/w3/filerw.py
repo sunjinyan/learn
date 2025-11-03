82f63df98dd90f23d125dcb6aa46317e46cbc005
sunjinyan <sunjinyan@zhikecn.com>
2025-11-03 22:01:09 +0800

    update

--- py/w3/filerw.py
+++ py/w3/filerw.py
@@ -4,6 +4,14 @@
 if __name__ == "__main__":
 
     # print("file os")
-    data =  open("test.txt",encoding= "utf8").read()
+    f = open("test.txt",encoding= "utf8",mode="w+")
 
+    # f.seek()
+    #
+    str1 = "老吾老,以及人之老,幼无幼，以及人之幼"
+    str2 = "不经贫贱难为人,不经世事永天真1"
+    f.write(f"{str1}\n{str2}")
+    f.flush()
+    data =  f.read()
     print(data)
+    f.close()
